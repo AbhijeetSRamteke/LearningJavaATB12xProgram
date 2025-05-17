23f2b5e35e929b6718ae7fb8aa944b90641d64ed
Abhijeet S. Ramteke <abhis.ramteke@gmail.com>
2025-05-17 16:18:17 +0530

    New change

--- src/Tasks/Task2_2_Max_Number.java
+++ src/Tasks/Task2_2_Max_Number.java
@@ -4,8 +4,8 @@ public class Task2_2_Max_Number
 {
     public static void main(String[] args)
     {
-        int a = 5, b = 7, c = 3;
-        String max = a > b ? "max number is "+a : (b > c ? "max number is "+b : "max number is "+c);
+        int N1 = 11, N2 = 12, N3 = 13;
+        String max = (N1>N2 && N1>N3) ? "Max number is "+N1 : (N2>N3 ? "Max number is "+N2 : "Max number is "+N3);
         System.out.println(max);
     }
 
