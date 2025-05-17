077fa5bc1f187f2a8dc4080f7759a6862b050658
Abhijeet S. Ramteke <abhis.ramteke@gmail.com>
2025-05-17 16:04:30 +0530

    New change

--- src/Tasks/Task2_1_Odd_Even.java
+++ src/Tasks/Task2_1_Odd_Even.java
@@ -5,7 +5,7 @@ public class Task2_1_Odd_Even
     public static void main(String[] args)
     {
         int a = 9;
-        String OddOREven = a%2 == 0 ? "Number is even" : "Number is Odd";
+        String OddOREven = (a%2 == 0) ? "Number is even" : "Number is Odd";
         System.out.println(OddOREven);
     }
 }
