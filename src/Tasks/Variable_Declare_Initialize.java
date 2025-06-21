0036de7298ca5041c104f9db7bfb8f581162ce65
Abhijeet S. Ramteke <abhis.ramteke@gmail.com>
2025-06-21 11:45:56 +0530

    New change

---
+++ src/Tasks/Variable_Declare_Initialize.java
@@ -0,0 +1,26 @@
+package Tasks;
+
+public class Variable_Declare_Initialize
+{
+    public static void main(String[] args)
+    {
+        int number = 30;
+        System.out.println("Number :" + number);
+
+        double abc = 29.99;
+        System.out.println("double :" + abc);
+
+        float num = 12.5f;
+        System.out.println("float :" + num);
+
+        long numl = 123456789l;
+        System.out.println("long number :" + numl);
+
+        boolean name = true;
+        System.out.println("boolean : " + name);
+
+        char xyz = 'A';
+        System.out.println("Character :" + xyz);
+
+    }
+}
